fd361dbe0503fa920d29c9258892d87651f3b47b
liordavidlevy <liordavid20@gmail.com>
2024-11-25 16:56:41 +0200

    fix: add development with temp token

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    spotifyAccessToken: 'BQDAoZ4r1FiguX_vvb1xTusRd_6u_5P043Ed2geSkdpvG2X2xUATP4TXHAaB9t--ZdqHp8wEVr0PtqD4zEaBqurvxtt2Um2McDrIoLEcInydehAGWe4' // Warning: valid until 17:50
+};
